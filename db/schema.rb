afd82b886c326902395a7c268dd1f7d5dd07cb0f
yerrip <yportill@lesley.edu>
2018-07-21 19:33:06 -0400

    Added new form params and seo friendly urls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_000602) do
+ActiveRecord::Schema.define(version: 2018_07_21_231718) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_07_09_000602) do
     t.string "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
