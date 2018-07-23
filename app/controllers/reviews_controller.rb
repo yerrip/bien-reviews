9d7d35adcf39476697e9c338dbd937825ae8d859
yerrip <yportill@lesley.edu>
2018-07-22 20:02:17 -0400

    Added Filters Feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,77 +1,81 @@
 class ReviewsController < ApplicationController
+    def index
+      #this is our list page for reviews
 
+      @price = params[:price]
+      @cuisine = params[:cuisine]
 
-  def index
-    #this is our list page for reviews
+      #start with all the reviews
+      @reviews = Review.all
 
+      #filtering by price
 
-  @number = rand(100)
+      if @price.present?
+        @reviews = @reviews.where(price: @price)
+      end
 
-  @reviews = Review.all
+      #filter by cuisine
+      if @cuisine.present?
+        @reviews = @reviews.where(cuisine: @cuisine)
+      end
+        
+    end
 
-  end
+    def new
+      #form for adding a new review
+      @review = Review.new
+    end
 
-  def new
-    #form for adding a new review
-    @review = Review.new
-  end
+    def create
+      #take the info from the form and add to the model
+      @review = Review.new(form_params)
 
-  def create
-    #take the info from the form and add to the model
-    @review = Review.new(form_params)
+      #we want to check if the model can be saved
+      #if it is we're going to the homepage again
+      #if it isn't show the new form
 
-    #we want to check if the model can be saved
-    #if it iswe're going to the homepage again
-    #if it isn't show the new form
+      if @review.save
+        redirect_to root_path
+      else
+        #show the view for new.html.erb
+        render "new"
+      end
+    end
 
-    if @review.save
+    def show
+      #individual review page
+      @review = Review.find(params[:id])
+    end
+
+    def destroy
+      #find the individual review
+      @review = Review.find(params[:id])
+      #destroy it
+      @review.destroy
+      #redirect to the homepage
       redirect_to root_path
     end
 
-  else
-    #show the view for new.html.erb
-    render "new"
-
-  end
-
-  def show
-
-    #individual review page
-    @review = Review.find(params[:id])
-
-  end
-
-  def destroy
-    #find the individual review
-    @review = Review.find(params[:id])
-    #destroy it
-    @review.destroy
-    #redirect to the homepage
-    redirect_to root_path
-
-  end
-
-  def edit
-    #find the individual review to edit
-    @review = Review.find(params[:id])
-
-  end
-
-
-  def update
-    #find individual review
-    @review = Review.find(params[:id])
-    #update with new info from form
-    if @review.update(form_params)
-      #redirect somewhere new
-      redirect_to review_path(@review)
-    else
-      render "edit"
+    def edit
+      #find the individual review to edit
+      @review = Review.find(params[:id])
     end
-  end
 
-  def form_params
-    params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance)
-  end
+
+    def update
+      #find individual review
+      @review = Review.find(params[:id])
+      #update with new info from form
+      if @review.update(form_params)
+        #redirect somewhere new
+        redirect_to review_path(@review)
+      else
+        render "edit"
+      end
+    end
+
+    def form_params
+      params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance, :cuisine, :price)
+    end
 
 end
